ad85d8457f97630556ded4d6eb309bfab27731e5
mawencheng <lingwei0604@163.com>
2017-03-13 20:11:45 +0800

    basic

---
+++ basic.py
@@ -0,0 +1,88 @@
+a = 100
+if a>= 0:
+	print(a)
+else:
+	print(-a)
+
+
+'I \'m\"ok\"!'  --> I'm "ok"!
+
+print('\\\t\\\')  -->\   \
+print(r'\\\t\\')  -->\\\t\\\
+
+
+print('''line1
+	...line2
+	...line3''')
+
+line1
+line2
+line3
+
+a = 1
+t_007 = 'T007'
+
+Answer = true
+
+PI = 3..3.333
+
+10//3 = 3
+10%3 = 1
+
+list 
+classmates = ['Michael','Bob','Tracy']
+
+append,pop,insert,
+
+
+classmates = ('Michael','Bob','Tracy')
+
+t = ()
+
+t = (1,)
+
+age = 20
+if age>=18
+	print("you age is",age)
+	print("adult")
+
+
+age = 3
+if age >=18
+	print("your age is ",age)
+else:
+	print("your age is ",age)
+
+
+age = 3
+if age >=18
+	print("")
+elif age >=4:
+	print()
+else
+	print
+
+
+s = input("birth")
+birth = int(s)
+if birth < 200:
+	print()
+else:
+	print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
